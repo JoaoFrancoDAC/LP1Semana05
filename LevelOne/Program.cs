121f8e1f2eda1d4a0a6f872025b95f8d71ce2232
JoaoFrancoDAC <jvfdc15@gmail.com>
2025-03-19 14:47:26 +0000

    added doc in main

--- LevelOne/Program.cs
+++ LevelOne/Program.cs
@@ -4,6 +4,10 @@ namespace LevelOne
 {
     public class Program
     {
+        /// <summary>
+        /// The program starts here
+        /// </summary>
+        /// <param name="args">Param given by the user</param>
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello LP1!");
