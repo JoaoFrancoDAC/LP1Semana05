4575cfa9e429c5662b9414f6294d814eb07ec440
JoaoFrancoDAC <jvfdc15@gmail.com>
2025-03-20 22:25:15 +0000

    fixed LFS tux

--- SoGoodLib/SoGoodClass.cs
+++ SoGoodLib/SoGoodClass.cs
@@ -8,5 +8,13 @@ namespace SoGoodLib
         {
             return "I came from another project!";
         }
+
+        public static int SoGoodFib(int n_number)
+        {
+            if (n_number <= 1)
+                return n_number;
+            else
+                return SoGoodFib(n_number - 1) + SoGoodFib(n_number - 2);
+        }
     }
 }
