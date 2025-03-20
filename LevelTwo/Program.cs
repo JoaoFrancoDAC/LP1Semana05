3cc681b25afee6d00296d4d5fb710ff02e48f1d4
JoaoFrancoDAC <jvfdc15@gmail.com>
2025-03-20 22:03:32 +0000

    added solution for the exercise 2

--- LevelTwo/Program.cs
+++ LevelTwo/Program.cs
@@ -7,9 +7,20 @@ namespace LevelTwo
     {
         private static void Main(string[] args)
         {
-            CanvasImage image = new CanvasImage("tux.jpg");
-            image.MaxWidth(20);
-            AnsiConsole.Write(image);
+            if (args.Length > 1) 
+            {
+                string imagem = args[0];
+                int largura = int.Parse(args[1]);
+                CanvasImage image = new CanvasImage(imagem);
+                image.MaxWidth(largura);
+                AnsiConsole.Write(image);
+            }
+            else if (args.Length == 0)
+            {
+                CanvasImage image = new CanvasImage("tux.jpg");
+                image.MaxWidth(24);
+                AnsiConsole.Write(image);
+            }
         }
     }
 }
